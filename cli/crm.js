e758d33285eb32a8810b4723628e7b7025978602
java-sparrow <x_l_st@126.com>
2014-05-07 23:19:36 +0800

    移植crm（入口文件）

--- cli/crm.js
+++ cli/crm.js
@@ -1,22 +1,100 @@
-/***************************************************************************
- * 
- * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
- * $Id$ 
- * 
- **************************************************************************/
- 
- 
- 
 /**
- * crm.js ~ 2014-05-06 23:11:05
- * @author xlst(x_l_st@126.com)
- * @version $Revision$ 
- * @description 
- * edp crm
- **/
+ * @file vm项目管理
+ * @author treelite(c.xinle@gmail.com)
+ */
+
+/**
+ * 元数据文件名
+ *
+ * @const
+ * @inner
+ * @type {string}
+ */
+var PROJECT_META =  'crmmetadata';
+
+
+/**
+ * jsDoc 配置文件
+ * @const
+ * @type {String}
+ */
+var DEFAULT_JSDOC_FILE = 'jsDoc.conf';
+
+/**
+ * 获取meta文件路径
+ */
+function getMetaDataFile() {
+    var project = require('edp-project');
+    var proInfoDir = project.getInfo().infoDir;
+    return require('path').resolve(proInfoDir, PROJECT_META);
+}
+
+/**
+ * 获取项目元数据
+ *
+ * @return {Object}
+ */
+exports.getMetaData = function () {
+    var metaFile = getMetaDataFile();
+
+    return require('./util').readJson(metaFile);
+};
+
+/**
+ * 设置项目元数据
+ *
+ * @param {Object} data
+ */
+exports.setMetaData = function (data) {
+    var util = require('./util');
+    var metaData = exports.getMetaData();
+
+    metaData = util.extend(metaData, data);
+
+    var metaFile = getMetaDataFile();
+
+    util.writeJson(metaFile, metaData);
+};
+
+/**
+ * 创建jsDoc配置文件
+ *
+ * @param {Object} projectInfo 项目信息
+ */
+exports.createJSDocConfFile = function (projectInfo) {
+    var tpl = require('./lib/scaffold').getTpl(
+            DEFAULT_JSDOC_FILE.replace(/\.conf$/, '')
+        );
+
+    var file = require('path').resolve(
+            projectInfo.dir,
+            DEFAULT_JSDOC_FILE
+        );
+
+    require('edp-codegen').text(tpl, {}, file);
+};
+
+/**
+ * 命令行配置项
+ */
 exports.cli = {
-    description: 'edp crm command description',
+    /**
+     * 命令描述信息
+     * 
+     * @type {string}
+     */
+    description: 'CRM项目管理',
+    /**
+     * 模块命令行运行入口
+     */
     main: function( args, opts ) {
-        console.log( 'See edp crm --help' );
+        var path = require('path');
+        var info = require('./lib/util').readJson(path.resolve(__dirname, '../package.json'));
+        
+        console.log('Hello RigelFE');
+        console.log('------');
+        console.log(info.name);
+        console.log('Version: ' + info.version);
+        console.log(info.description);
     }
 };
--- note/edpx-crm0.4.0移植笔记.md
+++ note/edpx-crm0.4.0移植笔记.md
@@ -6,3 +6,11 @@ edpx-crm移植笔记
 1. 更新`package.json`文件：增加所需依赖（`dependencies`属性）
 2. 使用`npm uninstall edpx-crm`命令卸载原有 **crm扩展**
 3. 切换至 *edpx-crm* 目录，使用`npm link .`命令重新安装 **crm扩展**（这样修改的`package.json`文件就能起作用，自动加载所需依赖至本地）
+
+
+- - - - - - - - - -
+
+
+移植 *cli/crm.js* 文件
+---------------------
+* `getMetaDataFile`方法中，使用`require('edp-project')`和`require('path')`来加载所需模块，但是这两种方式没法区分模块是**外部的**还是**内置的**。有没有比较好的编程指导或约定？（无法从模块名上区分，例如 *util.js* 文件里还使用了`require('mkdirp')`来加载外部模块）
